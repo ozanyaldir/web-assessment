7f6b28226872bf5f18752b15e714984f60a59b60
Ozan Yaldır <ozanyaldir@gmail.com>
2026-03-24 21:36:42 +0300

    Task1 completed!

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -24,27 +24,27 @@ useEffect(()=>{
     <div className='my-orders'>
         <h2>My Orders</h2>
         <div className="container">
-            {data.map((order, index)=>{
-                    return (
-                        <div key={index} className="my-orders-order">
-                            <img src={assets.parcel_icon} alt="" />
-                            <p>{order.items.map((item, index)=>{
-                                if(index === order.items.length-1){
-                                    return item.name+" x "+item.quantity
-                                }else{
-                                    return item.name+" x "+item.quantity + ","
-                                }
-                            })}</p>
-                            <p>${order.amount}.00</p>
-                            <p>Items: {order.items.length}</p>
-                            <p><span>&#x25cf;</span><b>{order.status}</b></p>
-                            <button onClick={fetchOrders}>Track Order</button>
-                        </div>
-                    )
+            {data.map((order, index) => {
+                return (
+                    <div key={index} className="my-orders-order">
+                        <img src={assets.parcel_icon} alt="parcel" />
+                        <p>
+                            {order.items.map((item, i) => (
+                                i === order.items.length - 1 
+                                ? `${item.name} x ${item.quantity}` 
+                                : `${item.name} x ${item.quantity}, `
+                            ))}
+                        </p>
+                        <p>${order.amount}.00</p>
+                        <p>Items: {order.items.length}</p>
+                        <p><span>&#x25cf;</span> <b>{order.status}</b></p>
+                        <button onClick={fetchOrders}>Track Order</button>
+                    </div>
+                )
             })}
         </div>
     </div>
-  )
+)
 }
 
 export default MyOrders
